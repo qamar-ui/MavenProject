b3d7328f4b7fa126f550493647b1e19e923e542e
Qamar <Qamar@LeLafe_Qamar>
2020-07-29 17:17:41 +0530

    comit POM

---
+++ MySeleniumBasesProjects/src/main/java/LaunchChrome/LaunchBrowser.java
@@ -0,0 +1,27 @@
+package LaunchChrome;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String s;
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		
+		WebDriver driver =new FirefoxDriver();
+		 driver.get("http://www.google.com");
+		 System.out.println(driver.getTitle());
+		 s=driver.getTitle();
+		 
+		 if (s=="google")
+			 System.out.println("Test case passed");
+		 else
+			 System.out.println("Test case failed");
+	}
+
+}
+

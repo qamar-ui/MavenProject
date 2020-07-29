efb2f0c77f6ca53b6553a994f310de37cfef5402
Qamar <Qamar@LeLafe_Qamar>
2020-07-29 16:45:02 +0530

    comit Myselenium

---
+++ MySeleniumBasesProjects/src/main/java/MySeleniumBasesProjects/MySeleniumBasesProjects/App.java
@@ -0,0 +1,13 @@
+package MySeleniumBasesProjects.MySeleniumBasesProjects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

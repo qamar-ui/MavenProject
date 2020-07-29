b92f47850f9b283157385a6c43e9729c141be5a9
Qamar <Qamar@LeLafe_Qamar>
2020-07-29 18:00:18 +0530

    first commit

---
+++ No pro/src/Pack1/LaunchBr.java
@@ -0,0 +1,32 @@
+
+package Pack1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchBr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String s;
+		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		
+		WebDriver driver =new FirefoxDriver();
+		 driver.get("http://www.google.com");
+		 System.out.println(driver.getTitle());
+		 s=driver.getTitle();
+		 
+		 if (s=="Google")
+		 {
+			 System.out.println("Test case passed");
+		 }
+		 else
+		 {
+			 System.out.println("Test case failed");
+	}
+
+}
+}
+
